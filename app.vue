5738840d621a1d3450d0bab009a102e8d394058a
PetiDev <peteristvanh@gmail.com>
2024-03-13 10:46:37 +0100

    feat: :sparkles: TitleComponent

--- app.vue
+++ app.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Title>Trefort Diákönkormányzat</Title>
+  <main>
+    <NavbarComponent />
+    <TitleComponent />
+  </main>
 </template>
+
+<style scoped>
+main {
+  min-height: 100vh;
+}
+</style>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap");
+
+:root {
+  --clr-main-purple: #451DE3;
+  --clr-dark-purple: #1E004B;
+  --clr-secondary-purple: #6D5E84;
+  --clr-gray-text: #8F8F8F;
+
+}
+
+* {
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  font-size: 15rem;
+}
+
+.rounded-30 {
+  border-radius: 30px;
+}
+
+a {
+  text-decoration: none;
+  color: var(--clr-secondary-purple);
+}
+
+a.active {
+  color: var(--clr-main-purple);
+}
+</style>
---
+++ components/titleComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <section>
+        <img id="title" src="/titles/trefort.svg">
+        <img moveable id="stabImg" class="rounded-30" src="/img/stab.bmp?url" alt="stab">
+        <img moveable id="lolImg" class="rounded-30" src="/img/lol.bmp?url" alt="lol">
+        <img moveable id="kotelImg" class="rounded-30" src="/img/kotel.bmp?url" alt="kotel">
+
+    </section>
+</template>
+
+<style scoped>
+img {
+    width: 300px;
+}
+
+section {
+    position: relative;
+    display: grid;
+    place-items: center;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    width: 100vw;
+    height: 80vh;
+}
+
+h1 {
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 1);
+    position: relative;
+    z-index: 10;
+    color: var(--clr-main-purple);
+}
+
+
+#title {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 890.622px;
+    transform: translate(-50%, -50%);
+}
+</style>
