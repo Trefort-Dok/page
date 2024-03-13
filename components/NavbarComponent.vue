9695d560c2ed7f0839b20205aab46770e76a881a
PetiDev <peteristvanh@gmail.com>
2024-03-13 10:45:47 +0100

    feat: :sparkles: navbar

---
+++ components/NavbarComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <div>
+            <h3>BSC Trefort <br> Diákönkományzat</h3>
+        </div>
+        <nav>
+            <a class="active" href="">Lorem</a>
+            <a href="">Lorem</a>
+            <a href="">Lorem</a>
+            <a href="">Lorem</a>
+            <a href="">Lorem</a>
+        </nav>
+    </section>
+</template>
+
+<style scoped>
+section{
+    display: flex;
+    justify-content: space-between;
+    padding: 3rem;
+}
+nav{
+    display: flex;
+    gap: 1rem;
+}
+</style>
